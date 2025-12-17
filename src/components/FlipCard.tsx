8842255c4798a4347d1149efcb7ee029e5a9475f
nikolai <sportislife635@gmail.com>
2025-12-17 23:41:47 +0300

    remove card level ui(temp)

--- src/components/FlipCard.tsx
+++ src/components/FlipCard.tsx
@@ -29,18 +29,12 @@ export function FlipCard({ card, isFlipped, onFlip }: FlipCardProps) {
         >
           {/* Front Side */}
           <div className="flipcard__side flipcard__front">
-            <div className="flipcard__mb-4">
-              <LevelIndicator currentLevel={card.currentLevel as 0 | 1 | 2 | 3} size="large" />
-            </div>
             <p className="flipcard__text">{getLevelContent(card, true)}</p>
             <div className="flipcard__hint">Нажмите, чтобы увидеть ответ</div>
           </div>
 
           {/* Back Side */}
           <div className="flipcard__side flipcard__back">
-            <div className="flipcard__mb-4">
-              <LevelIndicator currentLevel={card.currentLevel as 0 | 1 | 2 | 3} size="large" />
-            </div>
             <p className="flipcard__text">{getLevelContent(card, false)}</p>
             <div className="flipcard__hint">Уровень {card.currentLevel + 1} из {card.levels.length}</div>
           </div>
