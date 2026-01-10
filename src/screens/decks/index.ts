d761863ddb87ba534d046a5c8d4183cf91d0f07d
Utygett <87730969+Utygett@users.noreply.github.com>
2026-01-11 02:31:12 +0300

    chore: remove legacy screens/decks
    Merge PR #21: remove legacy screens/decks

--- src/screens/decks/index.ts
+++
@@ -1,8 +0,0 @@
-export { CreateDeck } from '../../features/deck-create';
-export { EditDeck } from '../../features/deck-edit';
-export { AddDeck } from './add/AddDeck';
-
-export { DecksActionsContainer } from '../../features/decks-actions';
-export { DecksFlowContainer } from '../../features/decks-flow';
-export type { DecksActionsApi } from '../../features/decks-actions';
-export type { DecksFlowApi } from '../../features/decks-flow';
