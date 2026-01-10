1ef6f799907efc048ca9ce4b1131d285e47f82e0
Utygett <87730969+Utygett@users.noreply.github.com>
2026-01-10 23:01:15 +0300

    feat: extract EditDeck to features/deck-edit (FSD)

--- src/screens/decks/index.ts
+++ src/screens/decks/index.ts
@@ -1,5 +1,5 @@
 export { CreateDeck } from './CreateDeck';
-export { EditDeck } from './EditDeck';
+export { EditDeck } from '../../features/deck-edit';
 export { AddDeck } from './add/AddDeck';
 export { DecksActionsContainer } from './DecksActionsContainer';
 export { DecksFlowContainer } from './DecksFlowContainer';
