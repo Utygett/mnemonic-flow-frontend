1f100037e9af0bddbe088a9eba1f30e2dd490e53
Utygett <87730969+Utygett@users.noreply.github.com>
2026-01-11 13:14:57 +0300

    refactor: migrate radio-group/progress to shared/ui-kit

--- src/shared/ui-kit/index.ts
+++ src/shared/ui-kit/index.ts
@@ -6,6 +6,8 @@ export * from './dropdown-menu';
 export * from './form';
 export * from './input';
 export * from './label';
+export * from './progress';
+export * from './radio-group';
 export * from './select';
 export * from './sheet';
 export * from './switch';
