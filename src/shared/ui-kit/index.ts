0ac12cf53e42ef83c7c7e7e3c8390615d031abcd
Utygett <87730969+Utygett@users.noreply.github.com>
2026-01-11 13:12:05 +0300

    refactor: migrate label/form to shared/ui-kit

--- src/shared/ui-kit/index.ts
+++ src/shared/ui-kit/index.ts
@@ -2,5 +2,7 @@ export * from './avatar';
 export * from './button';
 export * from './dialog';
 export * from './dropdown-menu';
+export * from './form';
 export * from './input';
+export * from './label';
 export * from './sheet';
