ad8aaa2ea830d80d9571bb9e66f25a783559b13b
Utygett <87730969+Utygett@users.noreply.github.com>
2026-01-11 10:51:38 +0300

    docs: add FSD contract; refactor: use widgets/main-shell public API

---
+++ src/widgets/main-shell/index.ts
@@ -0,0 +1,4 @@
+export { MainShellContainer } from '../../app/main-shell';
+export { MainShellView } from '../../app/main-shell';
+export { MnemonicRootSwitch } from '../../app/main-shell';
+export * from '../../app/main-shell';
