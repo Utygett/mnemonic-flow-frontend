ad8c1ca33df42a58201f26190fdf0ae55de49f62
Utygett <87730969+Utygett@users.noreply.github.com>
2026-01-11 11:37:30 +0300

    refactor: move RatingButton and DifficultyRating into features/study-flow (keep components shim)

--- src/features/study-flow/index.ts
+++ src/features/study-flow/index.ts
@@ -1,2 +1,5 @@
 export { StudyFlowStateContainer } from './model/StudyFlowStateContainer';
 export type { StudyController } from './model/StudyFlowStateContainer';
+
+export type { DifficultyRating } from './model/difficultyRating';
+export { RatingButton } from './ui/RatingButton';
