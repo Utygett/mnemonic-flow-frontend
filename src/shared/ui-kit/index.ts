30738e39cf99a9fa86312a5acd0fa519b0e8c5d9
Utygett <87730969+Utygett@users.noreply.github.com>
2026-01-11 16:46:34 +0300

    refactor: migrate aspect-ratio to shared/ui-kit

--- src/shared/ui-kit/index.ts
+++ src/shared/ui-kit/index.ts
@@ -1,6 +1,7 @@
 export * from './accordion';
 export * from './alert';
 export * from './alert-dialog';
+export * from './aspect-ratio';
 export * from './avatar';
 export * from './badge';
 export * from './breadcrumb';
