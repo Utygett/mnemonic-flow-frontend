4cd70e11dff71ead0174f5d2f11d6468eb5d5544
Utygett <87730969+Utygett@users.noreply.github.com>
2026-01-11 13:27:36 +0300

    refactor: migrate carousel to shared/ui-kit

--- src/shared/ui-kit/index.ts
+++ src/shared/ui-kit/index.ts
@@ -7,6 +7,7 @@ export * from './breadcrumb';
 export * from './button';
 export * from './calendar';
 export * from './card';
+export * from './carousel';
 export * from './checkbox';
 export * from './collapsible';
 export * from './context-menu';
