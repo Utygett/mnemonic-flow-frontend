bb7c3139099b9222b946c48d5043f1d59d615504
Utygett <87730969+Utygett@users.noreply.github.com>
2026-01-11 13:10:05 +0300

    refactor: add ui-kit utils and migrate button/input

--- src/shared/ui-kit/index.ts
+++ src/shared/ui-kit/index.ts
@@ -1,3 +1,3 @@
-// Temporary barrel file.
-// Will be expanded as ui-kit components are migrated from `src/components/ui`.
 export * from './avatar';
+export * from './button';
+export * from './input';
