ddacf10d657c26431a8b76b67c7d9587eb80d4c2
Utygett <87730969+Utygett@users.noreply.github.com>
2026-01-11 16:45:06 +0300

    refactor: migrate toggle to shared/ui-kit

--- src/shared/ui-kit/index.ts
+++ src/shared/ui-kit/index.ts
@@ -40,5 +40,6 @@ export * from './switch';
 export * from './table';
 export * from './tabs';
 export * from './textarea';
+export * from './toggle';
 export * from './tooltip';
 export * from './use-mobile';
