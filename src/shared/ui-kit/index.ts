354dcfc04cbbe67e9cde9635f578bf1b3944bd29
Utygett <87730969+Utygett@users.noreply.github.com>
2026-01-11 13:24:59 +0300

    refactor: migrate skeleton/slider to shared/ui-kit

--- src/shared/ui-kit/index.ts
+++ src/shared/ui-kit/index.ts
@@ -21,6 +21,8 @@ export * from './scroll-area';
 export * from './select';
 export * from './separator';
 export * from './sheet';
+export * from './skeleton';
+export * from './slider';
 export * from './switch';
 export * from './table';
 export * from './tabs';
