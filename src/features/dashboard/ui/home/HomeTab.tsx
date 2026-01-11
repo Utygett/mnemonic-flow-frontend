86619918f901928eb00aaad2fbcf08845cc57fb6
Utygett <87730969+Utygett@users.noreply.github.com>
2026-01-11 09:38:38 +0300

    refactor: move pages/home components into features/dashboard

---
+++ src/features/dashboard/ui/home/HomeTab.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import type { Deck, Group, Statistics } from '../../../../types';
+import type { PersistedSession } from '../../../../utils/sessionStore';
+
+import { DashboardContainer } from '../DashboardContainer';
+
+type Props = {
+  statistics: Statistics;
+  decks: Deck[];
+  groups: Group[];
+  activeGroupId: string | null;
+
+  resumeCandidate: PersistedSession | null;
+  onResume: () => void;
+  onDiscardResume: () => void;
+
+  onGroupChange: (groupId: string | null) => void;
+  onCreateGroup: () => void;
+  onDeleteActiveGroup: () => void;
+
+  onStartStudy: () => void;
+  onDeckClick: (deckId: string) => void;
+  onOpenEditDeck: (deckId: string) => void;
+  onAddDeck: () => void;
+};
+
+export function HomeTab(props: Props) {
+  return (
+    <DashboardContainer
+      statistics={props.statistics}
+      decks={props.decks}
+      groups={props.groups}
+      activeGroupId={props.activeGroupId}
+      resumeCandidate={props.resumeCandidate}
+      onResume={props.onResume}
+      onDiscardResume={props.onDiscardResume}
+      onGroupChange={(id) => props.onGroupChange(id)}
+      onCreateGroup={props.onCreateGroup}
+      onDeleteActiveGroup={props.onDeleteActiveGroup}
+      onStartStudy={props.onStartStudy}
+      onDeckClick={props.onDeckClick}
+      onOpenEditDeck={props.onOpenEditDeck}
+      onAddDeck={props.onAddDeck}
+    />
+  );
+}
